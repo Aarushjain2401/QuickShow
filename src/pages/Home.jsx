5a92dfe1f84eafc2cfa0c1d972137229e86f0566
Aarushjain2401 <aarushjain2401@gmail.com>
2025-08-06 16:07:43 +0530

    deployed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import FeaturedSection from "../components/FeaturedSection";
+import TrailerSection from "../components/TrailerSection";
+import Footer from "../components/footer";
+const Home = ()=>{
+  return(
+    <>
+  <HeroSection/>
+  <FeaturedSection/>
+  <TrailerSection/>
+  <Footer/>
+    </>
+  )
+}
+export default Home
